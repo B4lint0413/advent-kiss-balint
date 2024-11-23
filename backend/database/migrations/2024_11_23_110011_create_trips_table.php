f6481656d01bb12b71a55cc4649cf0d34941fd1e
B4lint0413 <balu050413@gmail.com>
2024-11-23 11:19:56 +0000

    finished

---
+++ backend/database/migrations/2024_11_23_110011_create_trips_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("destination_id")->references("id")->on("destinations");
+            $table->string("title", 90);
+            $table->date("start_date");
+            $table->date("end_date");
+            $table->float("price");
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
